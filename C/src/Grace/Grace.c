f89f7633cb4b5c26f7cbc58724a73a89878bf191
Elio Bonutto <elio.bonutto@gmail.com>
2026-01-21 18:15:12 +0100

    feat: almost the end of Sully.c

--- C/src/Grace/Grace.c
+++ C/src/Grace/Grace.c
@@ -1,3 +1,9 @@
-#include <stdio.h>
+#include <stdio.h> // FILE, fopen(), fprintf(), fclose()
 
-Q()
+#define KID "Grace_kid.c"
+#define STR "#include <stdio.h> // FILE, fopen(), fprintf(), fclose()%c%c#define KID %c%s%c%c#define STR %c%s%c%c#define Q(x) int main(){FILE *fptr;fptr = fopen(KID, %cw%c);if (!fptr){return 1;}fprintf(fptr, STR, 10, 10, 34, KID, 34, 10, 34, STR, 34, 10, 34, 34, 10, 10, 10, 10, 10);fclose(fptr);return 0;}%c%c/* Poussez-vous, excusez-moi ! Poussez-moi excusez-vous ! */%c%cQ(x)%c"
+#define Q(x) int main(){FILE *fptr;fptr = fopen(KID, "w");if (!fptr){return 1;}fprintf(fptr, STR, 10, 10, 34, KID, 34, 10, 34, STR, 34, 10, 34, 34, 10, 10, 10, 10, 10);fclose(fptr);return 0;}
+
+/* Poussez-vous, excusez-moi ! Poussez-moi excusez-vous ! */
+
+Q(x)

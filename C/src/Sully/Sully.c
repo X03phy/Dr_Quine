31ac43ec9f6f80999cfda638f15cb249b79eb2ea
Elio Bonutto <elio.bonutto@gmail.com>
2026-01-21 18:34:05 +0100

    feat: end of Sully.c

--- C/src/Sully/Sully.c
+++ C/src/Sully/Sully.c
@@ -20,7 +20,7 @@ int main(void)
 	if (!fptr)
 		return (1);
 
-	str = "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$c%1$cint main(void)%1$c{%1$c%2$cint i;%1$c%2$cchar buf[50];%1$c%2$cFILE *fptr;%1$c%2$cchar *str;%1$c%1$c%2$ci = %3$d;%1$c%2$cif (i <= 0)%1$c%2$c%2$creturn (0);%1$c%1$c%2$csprintf(buf, %1$4cSully_%1$5cd.c%1$4c, i);%1$c%1$c%2$cfptr = fopen(name, %4$cw%4$c);%1$c%1$c%2$cif (!fptr)%1$c%2$c%2$creturn (1);%1$c%1$c%2$cstr = %4$c%6$s%4$c;%1$c%2$cfprintf(fptr, str, 10, 9, i, 34, str);%1$c%1$c%2$cfclose(fptr);%1$c%1$c%2$csprintf(buf, %4$cgcc Sully_%1$5c1$d.c -o Sully_%1$5c1$d && ./Sully_%1$5c1$d, i);%1$c%2$csystem(buf);%1$c%1$c%2$creturn (0);%1$c}%1$c";
+	str = "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$c%1$cint main(void)%1$c{%1$c%2$cint i;%1$c%2$cchar buf[50];%1$c%2$cFILE *fptr;%1$c%2$cchar *str;%1$c%1$c%2$ci = %3$d;%1$c%2$cif (i <= 0)%1$c%2$c%2$creturn (0);%1$c%1$c%2$csprintf(buf, %4$cSully_%5$cd.c%4$c, i);%1$c%1$c%2$cfptr = fopen(buf, %4$cw%4$c);%1$c%1$c%2$cif (!fptr)%1$c%2$c%2$creturn (1);%1$c%1$c%2$cstr = %4$c%6$s%4$c;%1$c%2$cfprintf(fptr, str, 10, 9, (i - 1), 34, 37, str);%1$c%1$c%2$cfclose(fptr);%1$c%1$c%2$csprintf(buf, %4$cgcc Sully_%5$c1$d.c -o Sully_%5$c1$d && ./Sully_%5$c1$d%4$c, i);%1$c%2$csystem(buf);%1$c%1$c%2$creturn (0);%1$c}%1$c";
 	fprintf(fptr, str, 10, 9, (i - 1), 34, 37, str);
 
 	fclose(fptr);

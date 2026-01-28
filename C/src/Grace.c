561535658461d54be5f48496d80722a06154a455
X03phy <elio.bonutto@gmail.com>
2026-01-28 17:09:18 +0100

    fix: removing argument for macro Q

--- C/src/Grace.c
+++ C/src/Grace.c
@@ -1,9 +1,9 @@
 #include <stdio.h> // FILE, fopen(), fprintf(), fclose()
 
 #define KID "Grace_kid.c"
-#define STR "#include <stdio.h> // FILE, fopen(), fprintf(), fclose()%1$c%1$c#define KID %2$c%3$s%2$c%1$c#define STR %2$c%4$s%2$c%1$c#define Q(x) int main(){FILE *fptr;fptr = fopen(KID, %2$cw%2$c);if (!fptr){return 1;}fprintf(fptr, STR, 10, 34, KID, STR);fclose(fptr);return 0;}%1$c%1$c/* Main system, switching to normal mode. */%1$c%1$cQ(x)%1$c"
-#define Q(x) int main(){FILE *fptr;fptr = fopen(KID, "w");if (!fptr){return 1;}fprintf(fptr, STR, 10, 34, KID, STR);fclose(fptr);return 0;}
+#define STR "#include <stdio.h> // FILE, fopen(), fprintf(), fclose()%1$c%1$c#define KID %2$c%3$s%2$c%1$c#define STR %2$c%4$s%2$c%1$c#define Q int main(){FILE *fptr;fptr = fopen(KID, %2$cw%2$c);if (!fptr){return 1;}fprintf(fptr, STR, 10, 34, KID, STR);fclose(fptr);return 0;}%1$c%1$c/* Main system, switching to normal mode. */%1$c%1$cQ%1$c"
+#define Q int main(){FILE *fptr;fptr = fopen(KID, "w");if (!fptr){return 1;}fprintf(fptr, STR, 10, 34, KID, STR);fclose(fptr);return 0;}
 
 /* Main system, switching to normal mode. */
 
-Q(x)
+Q

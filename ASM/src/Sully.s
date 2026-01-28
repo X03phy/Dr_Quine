94cbe4ec239fe5cda7563f98ce8a0fe31a327fec
X03phy <elio.bonutto@gmail.com>
2026-01-29 00:22:06 +0100

    feat: nearly the end champ

--- ASM/src/Sully.s
+++ ASM/src/Sully.s
@@ -2,7 +2,7 @@ section .data
 	holy_variable: dq 5
 	filename_format: db "Sully_%d.s", 0
 	mode: db "w" 0
-	code_format: db "section .bss%1$c%2$cbuf: resb 200%1$csection .text%1$c%2$cglobal main%1$c%2$cextern sprintf%1$c%2$cextern fopen%1$c%2$cextern fprintf%1$c%2$cextern fclose%1$c%2$cextern system%1$cmain:%1$c%2$cpush   rbx%1$c%2$cdec    dword [rel holy_variable]%1$c%2$clea    rdi, [rel buf]%1$c%2$clea    rsi, [rel filename_format]%1$c%2$cmov    edx, dword [rel holy_variable]%1$c%2$ccall   sprintf wrt ..plt%1$c%2$clea    rdi, [rel buf]%1$c%2$clea    rsi, [rel mode]%1$c%2$ccall   fopen wrt ..plt%1$c%2$cjnz    .file_ok%1$c%2$cmov    eax, 1%1$c%2$cjmp    .ret%1$c.file_ok:%1$c%2$cmov    rbx, rax%1$c%2$clea    rdi, [rel buf]%1$c%2$cmov    rdi, rbx%1$c%2$clea    rsi, [rel code_format]%1$c%2$clea    edx, 10%1$c%2$clea    ecx, 9%1$c%2$cmov    r8d, dword [rel holy_variable]%1$c%2$cmov    r9d, 34%1$c%2$clea    r10, [rel code_format]%1$c%2$cpush   r10%1$c%2$csub    rsp, 8%1$c%2$ccall   fprintf wrt ..plt%1$c%2$cadd    rsp, 8%1$c%2$cpop    r10%1$c%2$cmov    rdi, rbx%1$c%2$ccall   fclose wrt ..plt%1$c%2$ccmp    eax, 0%1$c%2$cjge    .exec%1$c%2$cxor    eax, eax%1$c%2$cjl     .ret%1$c.exec:%1$c%2$clea    rdi, [rel buf]%1$c%2$clea    rsi, [rel exec_format]%1$c%2$cmov    edx, dword [rel holy_variable]%1$c%2$ccall   sprintf wrt ..plt%1$c%2$clea    rdi, [rel buf]%1$c%2$ccall   system wrt ..plt%1$c%2$cxor    eax, eax%1$c.ret:%1$c%2$cpop    rbx%1$c%2$cret%1$c", 0
+	code_format: db "" 0
 	exec_format: db "nasm -f elf64 Sully_%1$d.s -o Sully_%1$d.o && gcc Sully_%1$d.o -o Sully_%1$d", 0
 
 section .bss
@@ -35,7 +35,6 @@ main:
 .file_ok:
 	mov    rbx, rax
 
-	lea    rdi, [rel buf]
 	mov    rdi, rbx
 	lea    rsi, [rel code_format]
 	lea    edx, 10
@@ -44,14 +43,21 @@ main:
 	mov    r9d, dword [rel holy_variable]
 	lea    r10, [rel filename_format]
 	lea    r11, [rel mode]
-	push   r13
 	push   r12
+	push   r13
 	lea    r12, [rel code_format]
 	lea    r13, [rel exec_format]
-	call   fprintf wrt ..plt
-	push   r12
 	push   r13
-
+	push   r12
+	push   r11
+	push   r10
+	call   fprintf wrt ..plt
+	pop    r10
+	pop    r11
+	pop    r12
+	pop    r13
+	pop    r13
+	pop    r12
 	mov    rdi, rbx
 	call   fclose wrt ..plt
 

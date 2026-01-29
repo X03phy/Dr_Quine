04fa96206bc8edbc5d573f43b565f2d419363616
X03phy <elio.bonutto@gmail.com>
2026-01-29 13:19:04 +0100

    fix: buffer size to 200 and end condition fixed

--- ASM/src/Sully.s
+++ ASM/src/Sully.s
@@ -1,9 +1,9 @@
 section .data
 	holy_variable: dq 5
 	filename_format: db "Sully_%d.s", 0
-	mode: db "w" 0
-	code_format: db "" 0
-	exec_format: db "nasm -f elf64 Sully_%1$d.s -o Sully_%1$d.o && gcc Sully_%1$d.o -o Sully_%1$d", 0
+	mode: db "w", 0
+	code_format: db "section .data%1$c%2$choly_variable: dq %4$d%1$c%2$cfilename_format: db %3$c%5$s%3$c, 0%1$c%2$cmode: db %3$c%6$s%3$c, 0%1$c%2$ccode_format: db %3$c%7$s%3$c, 0%1$c%2$cexec_format: db %3$c%8$s%3$c, 0%1$c%1$csection .bss%1$c%2$cbuf: resb 200%1$c%1$csection .text%1$c%2$cglobal main%1$c%2$cextern sprintf%1$c%2$cextern fopen%1$c%2$cextern fprintf%1$c%2$cextern fclose%1$c%2$cextern system%1$c%1$cmain:%1$c%2$cpush   rbx%1$c%1$c%2$cdec    dword [rel holy_variable]%1$c%1$c%2$clea    rdi, [rel buf]%1$c%2$clea    rsi, [rel filename_format]%1$c%2$cmov    edx, dword [rel holy_variable]%1$c%2$ccall   sprintf wrt ..plt%1$c%1$c%2$clea    rdi, [rel buf]%1$c%2$clea    rsi, [rel mode]%1$c%2$ccall   fopen wrt ..plt%1$c%2$cjnz    .file_ok%1$c%2$cmov    eax, 1%1$c%2$cjmp    .ret%1$c%1$c.file_ok:%1$c%2$cmov    rbx, rax%1$c%1$c%2$cmov    rdi, rbx%1$c%2$clea    rsi, [rel code_format]%1$c%2$clea    edx, 10%1$c%2$clea    ecx, 9%1$c%2$cmov    r8d, 34%1$c%2$cmov    r9d, dword [rel holy_variable]%1$c%2$clea    r10, [rel filename_format]%1$c%2$clea    r11, [rel mode]%1$c%2$cpush   r12%1$c%2$cpush   r13%1$c%2$clea    r12, [rel code_format]%1$c%2$clea    r13, [rel exec_format]%1$c%2$cpush   r13%1$c%2$cpush   r12%1$c%2$cpush   r11%1$c%2$cpush   r10%1$c%2$ccall   fprintf wrt ..plt%1$c%2$cpop    r10%1$c%2$cpop    r11%1$c%2$cpop    r12%1$c%2$cpop    r13%1$c%2$cpop    r13%1$c%2$cpop    r12%1$c%2$cmov    rdi, rbx%1$c%2$ccall   fclose wrt ..plt%1$c%1$c%2$cmov    eax, dword [rel holy_variable]%1$c%2$ccmp    eax, 0%1$c%2$cjge    .exec%1$c%2$cxor    eax, eax%1$c%2$cjmp     .ret%1$c%1$c.exec:%1$c%2$clea    rdi, [rel buf]%1$c%2$clea    rsi, [rel exec_format]%1$c%2$cmov    edx, dword [rel holy_variable]%1$c%2$ccall   sprintf wrt ..plt%1$c%1$c%2$clea    rdi, [rel buf]%1$c%2$ccall   system wrt ..plt%1$c%1$c%2$cxor    eax, eax%1$c%1$c.ret:%1$c%2$cpop    rbx%1$c%2$cret%1$c", 0
+	exec_format: db "nasm -f elf64 Sully_%1$d.s -o Sully_%1$d.o && gcc Sully_%1$d.o -o Sully_%1$d && rm -f Sully_%1$d.o && ./Sully_%1$d", 0
 
 section .bss
 	buf: resb 200
@@ -18,6 +18,7 @@ section .text
 
 main:
 	push   rbx
+
 	dec    dword [rel holy_variable]
 
 	lea    rdi, [rel buf]
@@ -61,10 +62,11 @@ main:
 	mov    rdi, rbx
 	call   fclose wrt ..plt
 
+	mov    eax, dword [rel holy_variable]
 	cmp    eax, 0
 	jge    .exec
 	xor    eax, eax
-	jl     .ret
+	jmp     .ret
 
 .exec:
 	lea    rdi, [rel buf]
